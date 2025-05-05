945206c2eedaa101aa74cb6a783f4b63218ec094
nabilChemkhi <nabil.chemkhi@esprit.tn>
2025-05-05 12:39:17 +0100

    js s2

---
+++ 9-JS/app.js
@@ -0,0 +1,26 @@
+console.log("Hello World from app/js!");
+
+let price = 100;
+let name ='Nabil';
+let isAvalible = true;
+
+
+
+
+
+console.log('value of price:',priceprice);  // 100
+console.log('value of name:',    name); // Nabil
+console.log( 'value of isAvalible:',isAvalible);    // true
+console.log('type of price:',typeof price);  // number
+console.log( 'type of name:',typeof name);   // string
+console.log( 'type of isAvalible:',typeof isAvalible); // boolean
+console.log( 'type of price.toString():',typeof price.toString());   // string
+
+let color;
+console.log('value of color:',color); // undefined
+console.log("type of color :",typeof color); // undefined
+
+let person = null;
+console.log('value of person:',person); // null
+console.log( 'type of person:',typeof person); // object
+
