d09b322dc4610827deb40cafa724bce2914c04fa
nabilChemkhi <nabil.chemkhi@esprit.tn>
2025-06-02 12:51:14 +0100

    react ntro s1

--- 21-react js 1/reactFirstProject/src/App.jsx
+++ 21-react js 1/reactFirstProject/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Message from './components/Message'
 
 const App = () => {
+
+  //logic in js
+  const name = 'Nabil'
+  const getName = () => {
+    return 'Nabil'
+  }
+
   return (
     <div>
-      app
+      {/* JSX */}
+      
+      <Message />
+      
+      <h1>Hello {name} {getName()} {2 + 2} </h1>
     </div>
   )
 }
