045d1a2c813bb1b2299f9eecacc966ee90920e64
nabilChemkhi <nabil.chemkhi@esprit.tn>
2025-06-09 12:44:09 +0100

    useState

--- 24-react js 4/reactFirstProject/src/App.jsx
+++ 24-react js 4/reactFirstProject/src/App.jsx
@@ -4,13 +4,10 @@ import Counter from './components/Counter';
 const App = () => {
   return (
     <div style={{ maxWidth: '800px', margin: '0 auto', padding: '20px' }}>
-    <h1 className='text-center'>React App</h1>
-
-    
-    <div>
-      <Counter />
-    </div>
-
+      <h1 className="text-center">React App</h1>
+      <div>
+        <Counter />
+      </div>
     </div>
   );
 };
