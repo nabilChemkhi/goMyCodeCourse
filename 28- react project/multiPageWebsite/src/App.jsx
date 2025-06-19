a39ac3421392dc6666ef493039c5e83ec6569c03
nabilChemkhi <nabil.chemkhi@esprit.tn>
2025-06-19 15:21:48 +0100

    react router module

---
+++ 28- react project/multiPageWebsite/src/App.jsx
@@ -0,0 +1,21 @@
+
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import {Routes, Route} from 'react-router-dom';
+import NotFount from './pages/NotFount';
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFount />} />
+    </Routes>
+  );
+}
+
+
+export default App
