276233c833dc920c4148217dc8cba0cc37172ce9
nabilChemkhi <nabil.chemkhi@esprit.tn>
2025-06-26 16:02:45 +0100

    book form

--- 31- React bookManagerApplication/bookManagerApp/src/components/BookForm.jsx
+++ 31- React bookManagerApplication/bookManagerApp/src/components/BookForm.jsx
@@ -1,20 +1,47 @@
 import React from 'react'
 const BookForm = () => {
+const [title, setTitle] = React.useState('');
+const [author, setAuthor] = React.useState('');
+const [year, setYear] = React.useState('');
+
+const handeleTitleChange= (e) => {
+  setTitle(e.target.value);
+}
+const handleAuthorChange = (e) => {
+  setAuthor(e.target.value);
+}
+const handleYearChange = (e) => {    
+  setYear(e.target.value);
+}
+
+
   return (
     <div className="book-form-container">
       <h2 className="book-form-title">Add a New Book</h2>
       <form className="book-form-form">
         <div>
           <label htmlFor="title">Title:</label>
-          <input type="text" id="title" name="title" required />
+              
+          <input 
+          value={title} 
+          onChange={handeleTitleChange}
+           type="text" id="title" name="title" required />
         </div>
         <div>
           <label htmlFor="author">Author:</label>
-          <input type="text" id="author" name="author" required />
+            
+          <input 
+          value={author}
+          onChange={handleAuthorChange}
+           type="text" id="author" name="author" required />
         </div>
         <div>
           <label htmlFor="year">Year:</label>
-          <input type="number" id="year" name="year" required />
+          
+          <input 
+          value={year}
+          onChange={handleYearChange}
+           type="number" id="year" name="year" required />
         </div>
         <button type="submit">Add Book</button>
       </form>
