84bfb972cc39578f58a2c8782d5dd0e8bf6a5570
nabilChemkhi <nabil.chemkhi@esprit.tn>
2025-07-28 12:41:39 +0100

    envi variables

--- 46-project mearn film store/index.js
+++ 46-project mearn film store/index.js
@@ -1,5 +1,12 @@
+require('dotenv').config();
 const express = require('express');
+const colors = require('colors');
 const app = express();
+
+console.log(process.env.NODE_ENV);
+console.log(process.env.PORT);
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 
@@ -7,6 +14,6 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode.` .bgBlue.white.bold);
 });
